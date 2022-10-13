68c0344886c5ddfc17b4a826de59230ce87b0d8b
Amanbel <amanuel11belachew@gmail.com>
2022-10-13 15:46:26 -0700

    c prog

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ * Description: program for printing something
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char *str = "\"Programming is like building a multilingual puzzle";
