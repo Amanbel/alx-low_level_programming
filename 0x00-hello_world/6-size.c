acc287e5b047586573d5c82ca70768859a4c86ca
Amanbel <amanuel11belachew@gmail.com>
2022-10-13 16:49:58 -0700

    c prog

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,15 @@ int main(void)
 
 	float floatType;
 
-	double doubleType;
+	long int liType;
+
+	long long int lliType;
 
 	char charType;
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of a char: %zu bytes\n", sizeof(charType));
+printf("Size of an int: %zu bytes\n", sizeof(intType));
+printf("Size of a long int: %zu bytes\n", sizeof(liType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(lliType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
