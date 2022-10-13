d3a8b6e9630ff5cb7466353152a056f660049558
Amanbel <amanuel11belachew@gmail.com>
2022-10-13 16:39:26 -0700

    c prog

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: program to output size of data types
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int intType;
+
+	float floatType;
+
+	double doubleType;
+
+	char charType;
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of char: %zu byte\n", sizeof(charType));
+	return (0);
+}
