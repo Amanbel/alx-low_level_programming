ade52a4f90b009dc7b69f7e6f87bb85da1213ded
Amanbel <amanuel11belachew@gmail.com>
2022-10-14 10:15:10 -0700

    c prog

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Discription: hexadecimal notations
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int he, de;
+
+	for (he = 48; he <= 57; he++)
+	{
+		putchar(he);
+
+		if (he == 57)
+		{
+			for (de = 97; de <= 122; de++)
+			{
+				putchar(de);
+			}
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
