44895753b555f497a0392e63d65a6ae39b8bc61d
Amanbel <amanuel11belachew@gmail.com>
2022-10-14 11:19:15 -0700

    c prog

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Discription: nested if statment and for loop
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			if (a < b)
+			{
+			putchar(a);
+			putchar(b);
+			if (a != 56 && b != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
