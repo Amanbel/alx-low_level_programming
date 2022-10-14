3715e09552d3f2e6a819b0d05a6e8762005629a6
Amanbel <amanuel11belachew@gmail.com>
2022-10-14 07:56:56 -0700

    c prog

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.c>
+
+/**
+ * main - Entry Point
+ * Description: print lower case alphabets
+ * Return: 0 (Success)
+ */
+
+int main (void)
+{
+	int alp;
+
+	for (alp = "a";alp<="z";alp++)
+	{
+		putchar(alp);
+	}
+
+	putchar(10);
+
+	return (0);
+}
