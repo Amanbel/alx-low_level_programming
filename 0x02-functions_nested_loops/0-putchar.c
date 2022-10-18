d7dfba42ae41bf7b3cfb644d69b8be22dfd354e1
Amanbel <amanuel11belachew@gmail.com>
2022-10-18 11:09:20 -0700

    git func

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Discription: print _putchar
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar(10);
+
+	return (0);
+}
