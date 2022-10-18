773b4d336b3ee9f0bdf673756c5b29e9c554f637
Amanbel <amanuel11belachew@gmail.com>
2022-10-18 14:24:46 -0700

    c func

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Discription: print alphabet
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	putchar(10);
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i<=122)
+	{
+		putchar(i);
+		i++;
+	}
+
+	return;
+}
 void printall(void)
 {
 
