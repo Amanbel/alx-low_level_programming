9c54eda22694c765d43e59e446a1a83d485318b7
Amanbel <amanuel11belachew@gmail.com>
2022-10-19 00:59:14 -0700

    c func

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,19 +15,3 @@ void print_alphabet(void)
 	putchar(10);
 
 }
-
-void printall(void)
-{
-
-	putchar(95);
-	putchar(112);
-	putchar(117);
-	putchar(116);
-	putchar(99);
-	putchar(104);
-	putchar(97);
-	putchar(114);
-	putchar(10);
-
-	return;
-}
