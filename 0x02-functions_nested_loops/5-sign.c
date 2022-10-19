589315cb5a20ad82d1c9ae6cca223cd093974d11
Amanbel <amanuel11belachew@gmail.com>
2022-10-19 10:43:07 -0700

    c func

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
 		return (1);
+		putchar(43);
 	}
 	else if (n < 0)
 	{
-		putchar(45);
 		return (-1);
+		putchar(45);
 	}
 	else
 	{
-		putchar(48);
 		return (0);
+		puchar(48);
 	}
 }
