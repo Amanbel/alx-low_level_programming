6da471e1d863dae06b6afdf4b62e96bc4de1d1e4
Amanbel <amanuel11belachew@gmail.com>
2022-10-19 14:06:38 -0700

    c func

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Entry Point
+ * Discription: times table
+ * Return: Nothing
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if (b!= 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (c >= 10)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else if (c < 10 && b != 0)
+			{
+				_putchar(' ');
+				_putchar((c % 10) + '0');
+			}
+			else
+			{
+				_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
