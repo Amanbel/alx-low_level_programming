83522089797c44f917e2fd48a0095affea2e0412
Amanbel <amanuel11belachew@gmail.com>
2022-10-19 14:27:56 -0700

    c func

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Entry Point
+ * Discription: addition
+ * @a: number 1
+ * @b: number 2
+ * Return: Nothing
+ */
+
+int add(int a. int b)
+{
+	return (a + b);
+}
