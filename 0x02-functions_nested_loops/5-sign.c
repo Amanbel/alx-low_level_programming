afc0168a713f513c54ee5f646914f00fd314d59d
Amanbel <amanuel11belachew@gmail.com>
2022-10-19 10:28:18 -0700

    c func

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.c>
+#include "main.h"
+
+/**
+ * print_sign - Entry Point
+ * Discription: prints the sign of number
+ * @n: entered number
+ * Return: Nothing
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar(45);
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		putchar(48);
+		return (0);
+	}
+}
