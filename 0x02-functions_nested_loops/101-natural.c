ef082a649dcdb5cdec2c4db4a1211a3c67d6a6b8
Amanbel <amanuel11belachew@gmail.com>
2022-10-19 15:44:39 -0700

    c func

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Discription: sum of multiples of 3 and 5
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int sum, i = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
