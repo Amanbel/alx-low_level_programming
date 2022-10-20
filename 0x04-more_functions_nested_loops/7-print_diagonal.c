9755a1799698cf188dd5a419a725ba257e8e37ff
Amanbel <amanuel11belachew@gmail.com>
2022-10-20 10:50:35 -0700

    more func

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,19 +16,16 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 	}
-
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j <= i; j++)
 		{
-
 			_putchar(' ');
 
 			if (i == j)
 			{
 				_putchar('\\');
 			}
-
 		}
 		_putchar('\n');
 	}
