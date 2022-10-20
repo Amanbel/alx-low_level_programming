e85b77abf10b05c4dcc497ed58fd63037e0accb0
Amanbel <amanuel11belachew@gmail.com>
2022-10-20 12:07:20 -0700

    more func

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - Entry Point
+ * Discription: printing triangle
+ * @size: dimensions
+ * Return: Nothing
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			_putchar(' ');
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
