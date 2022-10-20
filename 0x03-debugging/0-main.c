3e10e71d430d6ae3a85b282f8acf3d89c2f75966
Amanbel <amanuel11belachew@gmail.com>
2022-10-20 13:31:39 -0700

    more func

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Discription: test for function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
