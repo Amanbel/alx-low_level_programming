98521b8f004220f23aaf23653be1f95dfe41862e
Amanbel <amanuel11belachew@gmail.com>
2022-10-20 13:45:29 -0700

    more func

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry Point
- * Discription: test for function
- * Return: 0 (Success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
-	positive_or_negative(0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
