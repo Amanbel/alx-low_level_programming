5aa2488184fb703f9b9be98b22ca45254a472e74
Amanbel <amanuel11belachew@gmail.com>
2022-10-20 12:53:15 -0700

    more func

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - Entry Point
+ * Discription: print integer
+ * @n: integer value
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	_putchar(n + '0');
+}
