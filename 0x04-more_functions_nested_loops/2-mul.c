9c7523b511b89bad73ed91b13f9c50ae25870de7
Amanbel <amanuel11belachew@gmail.com>
2022-10-20 04:53:11 -0700

    more func

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry Point
+ * Discription: multiply num
+ * @a: first num
+ * @b: second num
+ * Return: Nothing
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
