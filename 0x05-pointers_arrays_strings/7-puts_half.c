8744c4683d60c27adcc0b12ea691d40142979a7c
Amanbel <amanuel11belachew@gmail.com>
2022-10-24 13:50:05 -0700

    PAS

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,24 @@
 void puts_half(char *str)
 {
 	int i;
-	int hlen = (strlen(str) - 1) / 2;
+	int hlen = (strlen(str)) / 2;
 	int len = strlen(str) - 1;
+	int leng = strlen(str);
+	int hleng = (strlen(str) - 1) / 2;
 
-	for (i = hlen - 1; i <= len; i++)
+	if (leng % 2 == 0)
 	{
-		putchar(str[i]);
+		for (i = hlen; i <= len; i++)
+	{
+			putchar(str[i]);
+	}
+	}
+	else
+	{
+		for (i = hleng; i <= len; i++)
+		{
+			putchar(str[i]);
+		}
 	}
 	putchar(10);
 }
