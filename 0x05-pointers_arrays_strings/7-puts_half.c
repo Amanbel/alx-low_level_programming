3e5fa81604478413f0ab1567cfc8301882306e6b
Amanbel <amanuel11belachew@gmail.com>
2022-10-24 13:29:15 -0700

    PAS

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int hlen = (strlen(str) - 1) / 2;
+	int len = strlen(str) - 1;
+
+	for (i = len; i > hlen; i--)
+	{
+		putchar(str[i]);
+	}
+	putchar(10);
+}
