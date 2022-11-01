4278fb4b697c4c9059c5533357561270901df0e2
Amanbel <amanuel11belachew@gmail.com>
2022-11-01 07:54:34 -0700

    morePAS

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,9 +15,12 @@ unsigned int _strspn(char *s, char *accept)
 	int n = strlen(s);
 	int m = strlen(accept);
 	int k = 0;
+	int pc;
 
 	for (i = 0; i < m; i++)
 	{
+		pc = k;
+
 		for (j = 0; j < n; j++)
 		{
 			if (accept[i] == s[j])
@@ -25,9 +28,9 @@ unsigned int _strspn(char *s, char *accept)
 				k++;
 			}
 		}
-		if (k == '0')
+		if (pc == k)
 		{
-			return (k);
+			break;
 		}
 
 	}
