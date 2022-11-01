e93bc6f23b6d5569a00c0ea5f6269ac6bd6f46dc
Amanbel <amanuel11belachew@gmail.com>
2022-11-01 03:11:21 -0700

    morePAS

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,25 +11,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i, j;
-	char *m;
+	int i;
 	int n = strlen(s);
-	int k = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		if (s[i] == c)
 		{
-			for (j = i; j < n; j++)
-			{
-				m[k] = s[j];
-				k++;
-				if (j == (n - 1))
-				{
-					m[k] = '\0';
-				}
-			}
-			return (m);
+			return (s + i);
 		}
 	}
 	return (NULL);
