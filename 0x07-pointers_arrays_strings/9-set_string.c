3d92fed20ae2203ef477afec8b7a8d46d6bda051
Amanbel <amanuel11belachew@gmail.com>
2022-11-01 15:08:02 -0700

    morePAS

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set-string - aet a pointer to a string
+ * @s: pointer
+ * @to: string
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
