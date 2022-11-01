69330efca955a879b1146577b62239c67dcc2841
Amanbel <amanuel11belachew@gmail.com>
2022-11-01 12:40:33 -0700

    morePAS

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strstr - finds the substring
+ * @haystack: where the substring is searched
+ * @needle: the substring itself
+ * Return: a pointer to the substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, l;
+	int z = 1;
+	int n = strlen(haystack);
+	int m = strlen(needle);
+	int d = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		l = i;
+		if (needle[d] == haystack[i])
+		{
+			while (z)
+			{
+				if (needle[d] == haystack[i])
+				{
+					i++;
+					d++;
+				}
+				else
+				{
+					if (d == (m - 1))
+					{
+						return (haystack + l);
+					}
+					i = l;
+					d = 0;
+					break;
+				}
+			}
+		}
+	}
+	return (NULL);
+}
