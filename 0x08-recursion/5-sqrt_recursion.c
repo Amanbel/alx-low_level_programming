ec75a7e4aeed8c8bf44d2055834aff9bab7b2e93
Amanbel <amanuel11belachew@gmail.com>
2022-11-02 15:24:21 -0700

    recur

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * test - test if their is a square root
+ * @n: integer that is campared to the input
+ * @m: input number
+ * Return: the square root
+ */
+
+int test(int n, int m)
+{
+	if (n * n == m)
+	{
+		return (n);
+	}
+	if (n * n > m)
+	{
+		return (-1);
+	}
+	return (test(n + 1, m));
+}
+
+/**
+ * _sqrt_recursion - return the square root
+ * @n: input number
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (test(1, n));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a number is prime or not
+ * @n: input number
+ * Return: 0 if even, 1 if prime
+ */
+
+int is_prime_number(int n)
+{
+
