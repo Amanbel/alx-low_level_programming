65dfc853bd57d6390c8c08c2c5d739d6fc9f85ed
Amanbel <amanuel11belachew@gmail.com>
2022-11-04 11:59:14 -0700

    args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @argc: count of argument
+ * @argv: content of argument
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+readme for the project argc and argv
