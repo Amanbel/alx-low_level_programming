dc8df12811390bce956d805b0bb860ccefe00138
Amanbel <amanuel11belachew@gmail.com>
2022-11-04 13:20:24 -0700

    args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * @argc: count of arg
+ * @argv: content of arg
+ * Return: 0 if Success, 1 if not
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(srgv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
