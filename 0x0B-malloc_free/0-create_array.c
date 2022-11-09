3e1b58c6d8e92295ddf64298d0a29105278bbed7
Amanbel <amanuel11belachew@gmail.com>
2022-11-09 01:49:01 -0800

    mall

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,14 +11,14 @@
 
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *p = malloc(size * sizeof(char));
-
 	if (size == 0)
 		return (NULL);
 
+	unsigned int i;
+	char *p = malloc(size * sizeof(char));
+
 	for (i = 0; i < size; i++)
 		*(p + i) = c;
-	free(p);
+
 	return (p);
 }
