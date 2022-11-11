6511d8b8718426ee17abc27d0473d8a50b6f2853
Amanbel <amanuel11belachew@gmail.com>
2022-11-11 11:13:46 -0800

    moremall

---
+++ 0x0C-more_malloc_free/0-malloc-checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: size of the memory
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+readme for more malloc and additional projects on calloc,realloc and free
