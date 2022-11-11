00b7e20e4dcc8043d31ce6a387ea494fe5e1acf9
Amanbel <amanuel11belachew@gmail.com>
2022-11-11 13:41:45 -0800

    moremall

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * string_nconcat - concatinates two strings
+ * taking n bytes from second string
+ * @s1: first str
+ * @s2: second str
+ * @n: bytes
+ * Return: pointer to the concatinated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j = 0;
+	char *p;
+	int m, o;
+
+	m = strlen(s1);
+	o = strlen(s2);
+	sum = m + o;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+
+	p = malloc(sum * sizeof(char) + 1);
+
+	if(!p)
+		return (NULL);
+
+	while (*(s1 + i))
+	{
+		*(p + i) = *(s1 + i);
+		if (*(s1 + i) == '\0')
+		{
+			if (*(s2 + j))
+			{
+				while (j < n)
+				{
+					*(p + i) = *(s2 + j);
+					if (*(s2 + j) == '\0')
+						return (p);
+					j++;
+					i++;
+				}
+				return (p);
+			}
+			return (p);
+		}
+		i++;
+	}
+	return (p);
+}
