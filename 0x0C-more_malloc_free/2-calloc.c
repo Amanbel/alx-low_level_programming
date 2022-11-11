114fda95446f073ad535b805050b28dc985becd9
Amanbel <amanuel11belachew@gmail.com>
2022-11-11 15:23:41 -0800

    more_mall

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates memory like malloc
+ * @nmemb: number of elements
+ * @size: element size
+ * Return: 0
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i = 0;
+
+	if (!nmemb || !size)
+		return (NULL);
+	
+	p = malloc(nmemb * size);
+
+	if (!p)
+		return (NULL);
+	while (i < (nmemb * size))
+		p[i++] = 0;
+	return (p);
+}
