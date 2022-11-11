2eb3ed5502c78e8d213014f26023b4ad5a2ce5de
Amanbel <amanuel11belachew@gmail.com>
2022-11-11 14:47:22 -0800

    more_mall'

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	sum = m + o;
 
-	p = malloc((sum + 1) * sizeof(char));
+	p = malloc((sum + 1) * sizeof(char *));
 
 	if(!p)
 		return (NULL);
