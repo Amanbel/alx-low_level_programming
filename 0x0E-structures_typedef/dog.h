45b60a646f635fe02d2dd8d380339af6c5bd5c54
Amanbel <amanuel11belachew@gmail.com>
2022-11-14 13:29:01 -0800

    pre_process

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - custom data type for dogs
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
