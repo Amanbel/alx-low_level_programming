3ab301f24613a275eea74dbb9b529bc8eebb02d7
Amanbel <amanuel11belachew@gmail.com>
2022-11-16 11:28:33 -0800

    ptr_func

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - compares two numbers
+ * @array: array to compare from
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: index of match
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+		return (-1);
+	}
+}
