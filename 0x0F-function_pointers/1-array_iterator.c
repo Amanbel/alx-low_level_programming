825d05dc435658b23031552daa0911c407e1c114
Amanbel <amanuel11belachew@gmail.com>
2022-11-16 11:00:55 -0800

    ptr_func

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates trough an array
+ * @array: array of ints
+ * @size: size of array
+ * @action: pointer to a function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array + i * sizeof(int));
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,4 +3,5 @@
 #include <stddef.h>
 int _putchar(int c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
