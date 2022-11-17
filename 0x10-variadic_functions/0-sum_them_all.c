561f9dcc8526a266100c0dad526fccbb1c132046
Amanbel <amanuel11belachew@gmail.com>
2022-11-17 10:06:45 -0800

    var_func

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,12 +9,12 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int sum;
+
 	va_list args;
 
 	va_start(args, n);
-
-	unsigned int i;
-	int sum;
 
 	if (n == 0)
 		return (0);
