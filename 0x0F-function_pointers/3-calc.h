23b1afc355a63331cd3e95ca28bd07730262b999
Amanbel <amanuel11belachew@gmail.com>
2022-11-17 00:11:42 -0800

    ptr_func

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,9 +1,6 @@
 #ifndef CALC_H
 #define CALC_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * struct op - Struct op
  *
@@ -23,5 +20,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
 #endif 
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,7 +9,7 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	int = 0;
+	int i = 0;
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
