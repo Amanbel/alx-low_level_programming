080b543c4cb6f1496567b125039b3f7fb58724dc
Amanbel <amanuel11belachew@gmail.com>
2022-11-17 10:21:48 -0800

    var_func

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers to display
+ * @separator: character put after each consucative numbers
+ * @n: number of variables
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+		if (i == (n - 1))
+			printf("\n");
+	}
+	va_end(args);
+}
