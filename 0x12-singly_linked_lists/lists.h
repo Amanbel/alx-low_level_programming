bdd45567d57022b941e09bcf6ff89f36d2639cea
Amanbel <amanuel11belachew@gmail.com>
2022-11-24 13:40:19 -0800

    list_prot

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
+/**
+ * struct list_func - singly linked list
+ * @str: string data
+ * @len: integer data
+ * @next: pointer to next link
+ */
+
+struct link_func
+{
+	char *str;
+	unsigned int len;
+	struct link_func *next;
+} lint_t;
 
 int _putchar(int c);
 size_t print_list(const list_t *h);
