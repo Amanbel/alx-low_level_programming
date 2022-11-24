7d08093594e11316c309bf57ec1b8356722cdf5c
Amanbel <amanuel11belachew@gmail.com>
2022-11-24 13:43:08 -0800

    prnt_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
