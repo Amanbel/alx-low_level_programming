ec4fd57def6cd6419ab7776ce4dee8ce1be2dedb
Amanbel <amanuel11belachew@gmail.com>
2022-11-24 13:20:16 -0800

    count_elem

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 2;
+	size_t i = 0;
 
-	if (h->str == NULL)
+	while (h != NULL)
 	{
-		printf("[0] (nil)\n");
-	}
-	else
-	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
 		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
 	}
 	return (i);
 }
