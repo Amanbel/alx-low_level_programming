8417573fb2d619dc73f32857967778068cdaaff2
Amanbel <amanuel11belachew@gmail.com>
2022-11-25 01:08:32 -0800

    add_lst

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,11 +17,17 @@ list_t *add_node(list_t **head, const char *str)
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
 		return (NULL);
+
 	temp->str = strdup(str);
+
 	while (str[count] != '\0')
 		count++;
+
 	temp->len = count;
+
 	temp->next = *head;
+
 	*head = temp;
+
 	return (temp);
 }
