f4b0b7e8d5d57c4338f9778e8b4126d76930022f
Amanbel <amanuel11belachew@gmail.com>
2022-11-25 07:41:16 -0800

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,16 +2,15 @@ section .text
 	global main
 
 main:
-	mov eax, 0x4	;sys_write
+	mov edx, len	;length of mess
+	mov ecx, msg	;message
 	mov ebx, 1	;standard output
-	mov ecx, msg	;message var
-	mov edx, len	;lenght of message
-	int 0x80
+	mov eax, 4	;sys_write call
+	int 0x80	;call karnel
 
-	mov eax, 0x1
-	mov ebx, 0
-	int 0x80
+	mov eax, 1
+	int 0x80	;call karnel
 
 section .data
-	msg db "Hello, Holberton", 0xa
-	len equ $-msg
+	msg db "Hello, Holberton", 0xa	;assigning bytes to mess
+	len equ $-msg	;length equal to mess
