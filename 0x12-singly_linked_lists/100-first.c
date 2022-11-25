55b767327011f796b99c98896f184828e0959dd4
Amanbel <amanuel11belachew@gmail.com>
2022-11-25 06:15:35 -0800

    prnt_before_main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,17 @@ void before(void) __attribute__((constructor));
 
 void before(void)
 {
-	printf("You're beat! and yet, you must
-			allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+/**
+ * main - Entry Point
+ * Description: print to standard output
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
 }
