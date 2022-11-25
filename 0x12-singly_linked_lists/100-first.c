e100d70ea42f9c4fcb969f5f355a2281988f6a27
Amanbel <amanuel11belachew@gmail.com>
2022-11-25 06:10:06 -0800

    prnt_before_main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * before - Entry Point
+ * Description: print before main
+ */
+
+void before(void) __attribute__((constructor));
+
+void before(void)
+{
+	printf("You're beat! and yet, you must
+			allow,\nI bore my house upon my back!\n");
+}
