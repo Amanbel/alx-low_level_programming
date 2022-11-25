1e7c227c879e87e1946a505b3eb885ea6654b47d
Amanbel <amanuel11belachew@gmail.com>
2022-11-25 06:21:08 -0800

    prnt_before

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void before(void) __attribute__((constructor));
 
 void before(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
