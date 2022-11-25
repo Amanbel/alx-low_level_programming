9d2c1c65bfb92c4b60e15b1da8e84ee6d9bd3832
Amanbel <amanuel11belachew@gmail.com>
2022-11-25 06:19:03 -0800

    lst

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,15 +11,3 @@ void before(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-
-/**
- * main - Entry Point
- * Description: print to standard output
- * Return: 0
- */
-
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
