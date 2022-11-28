ca3d132f5d5d644a00811d6934d0964f768cc903
Amanbel <amanuel11belachew@gmail.com>
2022-11-28 06:45:17 -0800

    lst_proj

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - print integer from linked list
+ * @h: singly linked list
+ * Return: unsigned int(size_t)
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
