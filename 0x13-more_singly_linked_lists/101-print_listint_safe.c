9ddb8967f5bab46ef4183b84e4100657e130a2ef
Amanbel <amanuel11belachew@gmail.com>
2022-11-29 14:10:14 -0800

    list_mor

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,31 +3,6 @@
 #include "lists.h"
 
 /**
- * find_listint - finds loop
- * @head: pointer to linked list
- * Return: address of node
- */
-
-listint_t *find_listint(listint_t *head)
-{
-	listint_t *ptr, *end;
-
-	if (head == NULL)
-		return (NULL);
-
-	for (end = head->next; end != NULL; end = end->next)
-	{
-		if (end == end->next)
-			return (end);
-
-		for (ptr = head; ptr != end; ptr = ptr->next)
-			if (ptr == end->next)
-				return (end->next);
-	}
-	return (NULL);
-}
-
-/**
  * print_listint_safe - prints nodes of linked list with addresses
  * @head: pointer to linked list
  * Return: number of nodes
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,31 +3,6 @@
 #include "lists.h"
 
 /**
- * find_listint - finds loop
- * @head: pointer to linked list
- * Return: address of node
- */
-
-listint_t *find_listint(listint_t *head)
-{
-	listint_t *ptr, *end;
-
-	if (head == NULL)
-		return (NULL);
-
-	for (end = head->next; end != NULL; end = end->next)
-	{
-		if (end == end->next)
-			return (end);
-
-		for (ptr = head; ptr != end; ptr = ptr->next)
-			if (ptr == end->next)
-				return (end->next);
-	}
-	return (NULL);
-}
-
-/**
  * free_listint_safe - frees list
  * @h: pointer to linked list
  * Return: num of nodes
