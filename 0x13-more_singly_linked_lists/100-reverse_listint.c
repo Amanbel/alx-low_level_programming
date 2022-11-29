e70f5f3e0950423e5327ca3817163d1c27705f69
Amanbel <amanuel11belachew@gmail.com>
2022-11-29 11:59:09 -0800

    list_more

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -16,8 +16,8 @@ listint_t *reverse_listint(listint_t **head)
 	{
 		next = (*head)->next;
 		(*head)->next = prev;
-		prev = head;
-		head = next;
+		prev = *head;
+		*head = next;
 	}
 	*head = prev;
 	return (*head);
