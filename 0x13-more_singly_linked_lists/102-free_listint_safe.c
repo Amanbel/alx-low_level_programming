e2ebb32b329ebf06eacf537dd83f24f5f7f10013
Amanbel <amanuel11belachew@gmail.com>
2022-11-29 14:39:21 -0800

    list_mor

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,67 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * find_listint - finds loop
- * @head: pointer to linked list
- * Return: address of node
- */
-
-listint_t *find_listint_f(listint_t *head)
-{
-	listint_t *ptr, *end;
-
-	if (head == NULL)
-		return (NULL);
-
-	for (end = head->next; end != NULL; end = end->next)
-	{
-		if (end == end->next)
-			return (end);
-
-		for (ptr = head; ptr != end; ptr = ptr->next)
-			if (ptr == end->next)
-				return (end->next);
-	}
-	return (NULL);
-}
-
-
-/**
- * free_listint_safe - frees list
- * @h: pointer to linked list
+ * free_listint_safe - free linked list
+ * @h: pointer to function
  * Return: num of nodes
  */
 
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *next, *loopnode;
-	size_t len;
-	int loop = 1;
+	listint_t *current, *runner, *head;
+	size_t h_i, r_i;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
-	loopnode = find_listint_f(*h);
-	for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
+	current = *h;
+	head = *h;
+	h_i = 0;
+
+	while (head != NULL)
 	{
-		len++;
-		next = (*h)->next;
-		if (*h == loopnode && loop)
+		runner = *h;
+		for (r_i = 0; r_i < h_i; r_i++)
 		{
-			if (loopnode == loopnode->next)
+			if (runner == current)
 			{
-				free(*h);
-				break;
+				*h = NULL;
+				return (h_i);
 			}
-			len++;
-			next = next->next;
-			free((*h)->next);
-			loop = 0;
+			runner = runner->next;
 		}
-		free(*h);
+		current = head->next;
+		free(head);
+		head = current;
+		h_i++;
 	}
 	*h = NULL;
-	return (len);
+	return (h_i);
 }
