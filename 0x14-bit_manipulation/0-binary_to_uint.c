1ae6fd0d21a26605c4006a28e85d099443ac3dc4
Amanbel <amanuel11belachew@gmail.com>
2022-12-01 12:26:03 -0800

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,26 +15,28 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int sum;
 	unsigned int i;
 	unsigned int len;
+	char *str;
 
 	if (b == NULL)
 		return (0);
 
+	strcpy(str, b);
 	sum = 0;
 	len = strlen(b);
 
 	for (i = 0; i < len; i++)
 	{
-		if (b[i] > 'A' && b[i] < 'Z')
+		if (str[i] > 'A' && str[i] < 'Z')
 		{
 			return (0);
 		}
-		if (b[i] > 'a' && b[i] < 'z')
+		if (str[i] > 'a' && str[i] < 'z')
 		{
 			return (0);
 		}
 		else
 		{
-			sum += (pow(2, i) * atoi(b[i]));
+			sum += (pow(2, i) * atoi(str[i]));
 		}
 	}
 	return (sum);
