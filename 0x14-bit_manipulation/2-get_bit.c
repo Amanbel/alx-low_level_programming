2790caeb76e3450cf6c6f4f397b8d22224b4b1c9
Amanbel <101459491+Amanbel@users.noreply.github.com>
2022-12-02 06:32:56 -0800

    Update 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * get_bit - gets bit value at an index
+ * @n: integer to get bit value from
+ * @index: index of n to get bit from
+ *
+ * Return: bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int size;
+
+	size = sizeof(n) * 8 - 1;
+
+	if (index > size)
+		return (-1);
+
+	return ((n >> index) & 1);
+
+}
