1f255bcc087e3f065dfdfb5bf5f86efe1e334917
Amanbel <amanuel11belachew@gmail.com>
2022-12-12 12:02:53 -0800

    sys_c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND | O_CREATE, 0644);
+	fd = open(filename, O_RDWR | O_APPEND | O_CREAT, 0644);
 
 	if (fd == -1)
 		return (-1);
