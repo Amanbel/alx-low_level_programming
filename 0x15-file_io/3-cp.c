1e439c1b2b7a275b74e05f3c3169fdc820c3db11
Amanbel <amanuel11belachew@gmail.com>
2022-12-13 07:46:33 -0800

    sys_c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,37 +14,33 @@ int main(int ac, char **av)
 	int fd1, fd2, wr_chk;
 	char buff[1024];
 
-	if (ac > 3)
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
-
 	fd1 = open(av[1], O_RDWR);
 	fd2 = open(av[2], O_WRONLY | O_TRUNC);
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit (98);
+		exit(98);
 	}
-	
 	if (fd2 == -1)
 		fd2 = open(av[2], O_WRONLY | O_CREAT, 0644);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit (99);
+		exit(99);
 	}
-
 	while ((wr_chk = read(fd1, buff, sizeof(buff))) > 0)
 	{
 		if (write(fd2, buff, wr_chk) != wr_chk)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit (99);
+			exit(99);
 		}
 	}
-
 	if (close(fd1) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
