ab086232a885361f56e4896c84f61972cad9137b
Amanbel <amanuel11belachew@gmail.com>
2023-01-12 02:13:39 +0300

    print_data

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,7 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint *ptr = malloc(sizeof(dlistint_t));
+	dlistint_t *ptr = malloc(sizeof(dlistint_t));
 	unsigned int i = 0;
 
 	if (ptr == NULL)
