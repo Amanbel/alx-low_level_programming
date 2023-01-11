fc14c83de2b44754583fe149564ddfba90054540
Amanbel <amanuel11belachew@gmail.com>
2023-01-12 02:10:50 +0300

    print_data

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - print doubly linked list data
+ * @h: doubly linked list
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint *ptr = malloc(sizeof(dlistint_t));
+	unsigned int i = 0;
+
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr = h;
+
+	if (ptr != NULL)
+	{
+		printf("%d", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+	return (0);
+}
