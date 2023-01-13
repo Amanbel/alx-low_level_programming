a7746b713fab6d875be8f4cd8ca41549964f0c7e
Amanbel <amanuel11belachew@gmail.com>
2023-01-13 04:33:58 +0300

    len_dlist

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - print doubly linked list data
+ * @h: doubly linked list
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
