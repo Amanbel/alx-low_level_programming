09f2e222ca607341f43b7953a95dab1a73666c8c
Amanbel <amanuel11belachew@gmail.com>
2023-01-13 16:03:45 +0300

    insert_@pos

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (ptr->next == NULL)
 		return (add_dnodeint_end(h, n));
-	
+
 	new->n = n;
 	new->next = ptr->next;
 	new->prev = ptr;
