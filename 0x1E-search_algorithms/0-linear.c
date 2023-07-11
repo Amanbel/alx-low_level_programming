4fd5c2a054e43af3c7d1769534335b9df2e463d1
Amanbel <amanuel11belachew@gmail.com>
2023-07-12 01:38:29 +0300

    linear algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -24,7 +24,9 @@ int linear_search(int *array, size_t size, int value)
 	{
 		if (array[i] == value)
 		{
-			return (i);
+			int j = i;
+
+			return (j);
 		}
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 	}
